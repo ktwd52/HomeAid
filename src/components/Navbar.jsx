05a02d4d76b56b9b9ea4d9637dd26e97d4f9e63e
nandinibootweb <nandinitimes1@gmail.com>
2024-09-02 21:21:25 +0200

    added Profile icon and user details

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,12 @@
 import { useContext } from "react";
 import { NavLink } from "react-router-dom";
 import { AuthContext } from "../context/AuthProvider";
+import {Dropdown, DropdownTrigger, DropdownMenu, DropdownItem, Avatar, User} from "@nextui-org/react";
 
 const Navbar = ({ links, showLogout = false }) => {
-  const { logout } = useContext(AuthContext);
+  const { user, profile, logout } = useContext(AuthContext);
+  console.log("Navbar", user);
+  
   return (
     <nav className="text-primary-content bg-yellow-200">
       {/* <div className="max-w-[60rem] m-auto flex justify-between items-center flex flex justify-between">  */}
@@ -19,7 +22,7 @@ const Navbar = ({ links, showLogout = false }) => {
           />
           <h1 className="text-5xl font-sans tracking-widest text-amber-600 drop-shadow-lg">HomeAid</h1>
         </div>
-        <div className="flex flex-row ">
+        <div className="flex flex-row items-center">
           {links.map((link) => (
             <div key={link.value} className="px-6 text-amber-600 font-bold hover:text-amber-800">
               <NavLink to={link.value}>{link.name}</NavLink>
@@ -27,7 +30,26 @@ const Navbar = ({ links, showLogout = false }) => {
           ))}
           {showLogout && (
             <div className="px-6 text-amber-600 font-bold hover:text-amber-800">
-              <NavLink onClick={logout}>Logout</NavLink>
+              {/* <NavLink onClick={logout}>Logout</NavLink> */}
+              <Dropdown placement="bottom-start">
+                <DropdownTrigger>
+                  <User
+                    as="button"
+                     avatarProps={{
+                       isBordered: true,
+                    //   src: "https://i.pravatar.cc/150?u=a042581f4e29026024d",
+                     }}
+                    className="transition-transform"
+                    description={`@${profile?.username}`}
+                    name={profile ? `${profile.firstname} ${profile.lastname}` : ''}
+                  />
+                </DropdownTrigger>
+                <DropdownMenu aria-label="User Actions" variant="flat">
+                  <DropdownItem key="logout" color="danger" onClick={logout}>
+                    Log Out
+                  </DropdownItem>
+                </DropdownMenu>
+              </Dropdown>
             </div>
           )}
         </div>
